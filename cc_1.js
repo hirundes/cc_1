502c4b8613a6e14b79e19125f952390c6bf5bf83
HIRUN <hirundesilva97@gmail.com>
2025-01-21 13:05:05 -0500

    Task 1 - Employee Information.

--- cc_1.js
+++ cc_1.js
@@ -1,4 +1,4 @@
-// Task 1 - Employee Information
+// Task 1 - Employee Information.
 let employeeName = "Jack";
 const employeeID = 23;
 var isActive = true;
@@ -10,9 +10,28 @@ console.log(typeof isActive)
 let productName = "NiceBottle";
 const productPrice = 15;
 var isAvailable = true;
-console.log(typeof productName)
-console.log(typeof productPrice)
-console.log(typeof isAvailable)
+console.log(productName)
+console.log(productPrice)
+console.log(isAvailable)
+
+//Task 3 - Financial Transactions
+let accountBalanace = 13;
+accountBalanace = accountBalanace + 5;
+console.log(accountBalanace)
+console.log(accountBalanace + 5)
+
+//Task 4 - Customer Messaging
+let customerName = "Mike";
+let welcome = "Hello";
+let welcomeMessage = welcome + customerName;
+console.log(customerName)
+console.log(welcomeMessage)
+
+//Task 5 - Access Control
+let isLoggedIn = true;
+console.log(false || true)
+
+
 
 
 
