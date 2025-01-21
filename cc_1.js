33538085d546cb8ac81187fe877d1ddce01a748f
HIRUN <hirundesilva97@gmail.com>
2025-01-21 12:09:57 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,14 @@ var isActive = true;
 console.log(typeof employeeName)
 console.log(typeof employeeID)
 console.log(typeof isActive)
+
+// Task 2 - Product Details
+let productName = "NiceBottle";
+const productPrice = 15
+var isAvailable = true;
+console.log(typeof productName)
+console.log(typeof productPrice)
+console.log(typeof isAvailable)
+
+
+
