9df57def67d18691199edcb8ff955ceccd4148e2
HIRUN <hirundesilva97@gmail.com>
2025-01-23 12:17:22 -0500

    Added Comments to Each Task

--- cc_1.js
+++ cc_1.js
@@ -2,6 +2,7 @@
 let employeeName = "Jack";
 const employeeID = 23;
 var isActive = true;
+     //Logged each variable with typeof
 console.log(typeof employeeName)
 console.log(typeof employeeID)
 console.log(typeof isActive)
@@ -10,24 +11,29 @@ console.log(typeof isActive)
 let productName = "NiceBottle";
 const productPrice = 15;
 var isAvailable = true;
+     //Logged each variable
 console.log(productName)
 console.log(productPrice)
 console.log(isAvailable)
 
 //Task 3 - Financial Transactions.
 let accountBalanace = 13; 
+     //Logged accountBalance
 console.log(accountBalanace)
+     //Performed Basic Arithmetic Operation and Logged Change
 console.log(accountBalanace + 5) 
 
 //Task 4 - Customer Messaging.
 let customerName = "Mike";
 let welcome = "Hello";
 let welcomeMessage = welcome + customerName;
+     //Logged customerName and welcomeMessage
 console.log(customerName)
 console.log(welcomeMessage)
 
 //Task 5 - Access Control.
 let isLoggedIn = true;
+     //Logged True or False
 console.log(false || true)
 
 
